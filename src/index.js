51fb0220acff8bbb7eeffafdaa5741ce7e728037
Igor <igorko.by@gmail.com>
2020-03-11 15:38:24 +0300

    feat: implement solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let symbol,
+    result = 0;
+
+  while (n) {
+    symbol = n % 10;
+    result = (result * 10) + symbol;
+    n = parseInt(n / 10);
+  };
+
+  return Math.abs(result);
 }
